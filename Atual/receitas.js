34cb82fb30d3418dc11a637783eb6452e29b22dd
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2023-04-11 22:18:23 -0300

    att trabalho

--- Atual/receitas.js
+++ Atual/receitas.js
@@ -1,18 +1,18 @@
 const receitas = [
     {
         titulo: "Arroz de Couve-Flor",
-        imagem: "#",
+        imagem: "../img/Arroz.jpg",
         preparo: "Deixe a couve-flor picada. Adicione os igredientes e refogue bem. Adicione sal, tampe a panela e deixe cozinhar.",
         ingredientes: [
             "Arroz",
             "Couve-Flor",
             "Cebola Média",
-            "Azeite",
+            "Azeite"
         ]
     },
     {
         titulo: "Bolo de Café",
-        imagem: "#",
+        imagem: "../img/Bolo.jpg",
         preparo: "Bata o açucar, as gemas e o café. Adicione farinha e chocolate e mexa bem. Bata as claras e junte á mistura.",
         ingredientes: [
             "Farinha de Trigo",
@@ -24,7 +24,7 @@ const receitas = [
     },
     {
         titulo: "Coxinha de Brigadeiro",
-        imagem: "#",
+        imagem: "../img/Coxinha.jpg",
         preparo: "Junte o Leite condensado, chocolate em pó e manteiga. Aqueça no fogo baixo. Envolva os morangos e passe no granulado.",
         ingredientes: [
             "Leite Condensado",
@@ -37,11 +37,37 @@ const receitas = [
 ]
 
 function getListaIngredientes (receitas){
-
+    let listaHTML = "<ul>";
+    const listaIngredientes = receitas.ingredientes.map((ingrediente) => {
+        return "<li>" + ingrediente + "</li>";
+      })
+      .reduce((acumulador, item) => {
+        return acumulador + item;
+      }, "");
+    listaHTML += listaIngredientes;
+    listaHTML += "</ul>";
+    return listaHTML;
 }
 function getCard(receitas){
-
+  const card = `
+    <div class="card" style="width: 250px;">
+      <img src="${receitas.imagem}" class="card-img-top" alt="${receitas.titulo}">
+      <div class="card-body">
+        <h5 class="card-title">${receitas.titulo}</h5>
+        <div class="card-text">${getListaIngredientes(receitas.ingredientes)}</div>
+        <hr>
+        <p>${receita.preparo}</p>
+      </div>
+    </div>
+  `;
+  return card;
 }
 function preencheCatalogo(){
+    const pnlCatalogo = document.getElementById("pnlCatalogo");
+
+    const receitasHtml = receitas.map(receita => getCard(receita)).reduce((html, cardHtml) => html + cardHtml, "");
+  
+    pnlCatalogo.innerHTML = receitasHtml;
     
 }
+
--- colinha.js
+++ colinha.js
@@ -1,7 +1,72 @@
+const receitas = [
+    {
+        titulo: "Arroz de Couve-Flor",
+        imagem: "../img/Arroz.jpg",
+        preparo: "Deixe a couve-flor picada. Adicione os igredientes e refogue bem. Adicione sal, tampe a panela e deixe cozinhar.",
+        ingredientes: [
+            "Arroz",
+            "Couve-Flor",
+            "Cebola Média",
+            "Azeite",
+        ]
+    },
+    {
+        titulo: "Bolo de Café",
+        imagem: "../img/Bolo.jpg",
+        preparo: "Bata o açucar, as gemas e o café. Adicione farinha e chocolate e mexa bem. Bata as claras e junte á mistura.",
+        ingredientes: [
+            "Farinha de Trigo",
+            "Açúcar",
+            "Café",
+            "Chocolate em Pó",
+            "Ovos"
+        ]
+    },
+    {
+        titulo: "Coxinha de Brigadeiro",
+        imagem: "../img/Coxinha.jpg",
+        preparo: "Junte o Leite condensado, chocolate em pó e manteiga. Aqueça no fogo baixo. Envolva os morangos e passe no granulado.",
+        ingredientes: [
+            "Leite Condensado",
+            "Chocolate Em Pó",
+            "Manteiga",
+            "Morango",
+            "Chocolate Granulado"
+        ]
+    },
+]
 
-const lista = [1, 2, 3, 4, 5];
-const ul = document.getElementById('valores');
+function getListaIngredientes (receitas){
+  let listaHTML = "<ul>";
+  const listaIngredientes = receitas.ingredientes.map((ingredientes) => {
+    return "<li>" + ingredientes + "</li>";
+  }).reduce((acumulador, item) => {
+    return acumulador + item; }, "");
+  listaHTML += listaIngredientes;
+  listaHTML += "</ul>";
+  return listaHTML;
 
-const listaHTML = lista.map(item => `<li>${item}</li>`).join('');
+}
+function getCard(receitas){
+  const card = `
+    <div class="card" style="width: 250px;">
+      <img src="${receitas.imagem}" class="card-img-top" alt="${receitas.titulo}">
+      <div class="card-body">
+        <h5 class="card-title">${receitas.titulo}</h5>
+        <div class="card-text">${getListaIngredientes(receitas.ingredientes)}</div>
+        <hr>
+        <p>${receita.preparo}</p>
+      </div>
+    </div>
+  `;
+  return card;
+}
+function preencheCatalogo(){
+    const pnlCatalogo = document.getElementById("pnlCatalogo");
 
-ul.innerHTML = listaHTML;
+    const receitasHtml = receitas.map(receita => getCard(receita)).reduce((html, cardHtml) => html + cardHtml, "");
+  
+    pnlCatalogo.innerHTML = receitasHtml;
+    
+}
+
